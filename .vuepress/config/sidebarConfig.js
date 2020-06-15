b4b7eb8dd6f668f072066e04f69b6ebd9a9eb721
夕水 <854806732@qq.com>
2020-06-15 21:56:17 +0800

    添加了一些标题，内容待编写

--- .vuepress/config/sidebarConfig.js
+++ .vuepress/config/sidebarConfig.js
@@ -33,4 +33,53 @@ module.exports = [
         collapsable: true,
         sidebarDepth:1
     },
+    {
+        title: '常用的编程概念',
+        path: '/doc/common-concept', 
+        collapsable: true,
+        children:[
+            {
+                title: '变量与可变性',
+                path:"/doc/variable-and-mutability",
+                collapsable: true
+            },
+            {
+                title: '数据类型',
+                path:"/doc/data-type",
+                collapsable: true
+            },
+            {
+                title: '函数',
+                path:"/doc/functions",
+                collapsable: true
+            },
+            {
+                title: '控制流',
+                path:"/doc/control-flow",
+                collapsable: true
+            },
+        ]
+    },
+    {
+        title: '明白所有权',
+        path: '/doc/understand-ownership', 
+        collapsable: true,
+        children:[
+            {
+                title: '什么是所有权',
+                path:"/doc/what-ownership",
+                collapsable: true
+            },
+            {
+                title: '引用与借用',
+                path:"/doc/references-and-borrow",
+                collapsable: true
+            },
+            {
+                title: '切片类型',
+                path:"/doc/slice-type",
+                collapsable: true
+            },
+        ]
+    },
 ]
