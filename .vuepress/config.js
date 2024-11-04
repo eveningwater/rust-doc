5072825cf0dd899eab03f5081b8b7cf38d76fc12
eveningwater <854806732@qq.com>
2024-11-04 09:14:04 +0000

    feat: 修改了打包配置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,7 @@ const navConfig = require('./config/navConfig.js')
 const sidebarConfig = require('./config/sidebarConfig.js')
 
 module.exports = {
-    dest: "./docs",
+    dest: "./dist",
     base: process.env.NODE_ENV === 'development' ? "/rust-doc/" : '/',
     head: [
         ['link', {
