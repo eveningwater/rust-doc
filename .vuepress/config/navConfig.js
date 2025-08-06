56808150af80a540a3fae178c53b020627c808a7
eveningwater.123 <854806732@qq.com>
2025-08-06 16:31:17 +0800

    feat: 更新了文档架构

--- .vuepress/config/navConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    { text: '首页', link: '/' },
-    { text: 'github', link: 'https://github.com/eveningwater/rust-doc' },
-    { text: '码云', link: 'https://gitee.com/eveningwater' },
-]
--- doc/appendix/appendix-f.md
+++
@@ -1,3 +0,0 @@
-## 附录 F：本书的翻译
-
-提供英语以外其他语言的资源。大多数仍在进行中；请参阅 [翻译标签](https://github.com/rust-lang/book/issues?q=is%3Aopen+is%3Aissue+label%3ATranslations) 来帮助或让我们知道新的翻译！
